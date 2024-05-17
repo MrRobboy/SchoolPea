0870b55b31c3f840b7ddc7ca4f22693383a4a835
matisu <150020403+MrRobboy@users.noreply.github.com>
2024-05-17 17:50:33 +0200

    le grand remplacment

--- public/index.php
+++ public/index.php
@@ -1,27 +1,11 @@
 <?php
+include_once('_config.php');
 
-// Inclusion des fichiers nécessaires
-require_once 'core/Router.php'; // Inclure la classe Router
-
-// Instanciation du routeur
-$router = new Router();
-
-// Définition des routes
-
-// Route pour la page d'accueil non connectée
-$router->get('/accueil-non-connecte', function() {
-    include 'app/views/pages/Accueil (non-connecté)/index.html';
-});
-
-// Route pour la page d'accueil connectée
-$router->get('/accueil-connecte', function() {
-    include 'app/views/pages/Accueil (connecté)/index.html';
-});
-
-// Route pour la page d'inscription/connexion
-$router->get('/inscription-connexion', function() {
-    include 'app/views/pages/Inscription-Connexion/index.html';
-});
-
-// Exécution du routeur
-$router->run();
+$request = $_GET['r']; //index.php?r...
+if($request == "accueil")
+{
+include_once(CONTROLLERS./'Accueil (non-connecté)/accueil.php');
+}else{
+    include(PUBLIC./'error404.php');
+}
+?>
