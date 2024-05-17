df4321aceec94222a441fe39b70c5d382c79ddfe
matisu <150020403+MrRobboy@users.noreply.github.com>
2024-05-17 14:02:37 +0200

    update

---
+++ app/views/includes/layout.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="fr-FR">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+        <title>Schoolpéa</title>
+        <link rel="stylesheet" type="text/css" href="./index.css" />
+        <script src="./index.js"></script>
+    </head>
---
+++ public/error404.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Erreur 403 - Accès Interdit</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            background-color: #f8f9fa;
+            margin: 0;
+            padding: 0;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            text-align: center;
+        }
+        .container {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        }
+        h1 {
+            color: #dc3545;
+            margin-bottom: 20px;
+        }
+        p {
+            color: #6c757d;
+            margin-bottom: 20px;
+        }
+        .cats-container {
+            display: flex;
+            justify-content: center;
+            margin-top: 20px;
+        }
+        .cat {
+            width: 150px;
+            margin: 0 10px;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Erreur 403 - Accès Interdit</h1>
+        <p>Oups ! Vous n'avez pas la permission d'accéder à cette page.</p>
+        <p>Mais ne vous inquiétez pas, voici quelques chats pour vous remonter le moral :</p>
+        <div class="cats-container">
+             <img class="cat" src="https://media.giphy.com/media/cfuL5gqFDreXxkWQ4o/giphy.gif" alt="Chat 1">
+            <img class="cat" src="https://media.giphy.com/media/MCfhrrNN1goH6/giphy.gif" alt="Chat 2">
+            <img class="cat" src="https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif" alt="Chat 3">
+        </div>
+    </div>
+</body>
+</html>
