ada76f4559c58d16d5e10b040bf21336be00f7e5
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-05-23 22:32:59 +0200

    truc

--- app/view/layout.php
+++ app/view/layout.php
@@ -5,16 +5,8 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Schoolpéa</title>
-    <link rel="stylesheet" href="<?echo CSS;?>style.css" />
-    <script src="<?echo JS;?>script.js"></script>
-    <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-            crossorigin="anonymous" />
-        <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" />
+    <link rel="stylesheet" href="<? echo CSS; ?>style.css" />
+    <script src="<? echo JS; ?>script.js"></script>
 </head>
 
 <body>
@@ -47,8 +39,8 @@
         </span>
     </header>
 
-<!--Ma page -->
-<?php echo $contentPage;?>
+    <!--Ma page -->
+    <?php echo $contentPage; ?>
 
     <footer>
         <div class="footer">
@@ -82,7 +74,7 @@
                 <a>Api fetch à Implémenter <br />Input email</a>
             </span>
         </div>
-</footer>
+    </footer>
 
 
 </body>
