9b09782495da928bf6fdf59fb8557398f22c5582
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-05-24 11:38:02 +0200

    VRAI FIN

--- app/view/logs.php
+++ app/view/logs.php
@@ -1,3 +1,9 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+    <title>Logs</title>
+    <link rel="stylesheet" type="text/css" href="./logs.css" />
+</head>
 
 <body>
     <div class="container">
