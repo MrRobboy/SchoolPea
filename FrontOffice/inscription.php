dc0dd685ae1cee9c2174f5e0fc70000346c02428
matisu <150020403+MrRobboy@users.noreply.github.com>
2024-05-29 18:04:07 +0200

    tiu

--- FrontOffice/inscription.php
+++ FrontOffice/inscription.php
@@ -1,4 +1,34 @@
-<?php include_once '../FrontEnd/inscription.php'; ?>
+<?php
+session_start();
+$bdd = new PDO('mysql: host=localhost; dbname=PA; charset=utf-8;', 'root' , 'root');
+if (isset($_POST['submit']))
+{
+    if(!empty($_POST['name']) and !empty($_POST['email']) and !empty($_POST['password']));
+    {
+        $name = htmlspecialchars($_POST['name']);
+        $email = htmlspecialchars($_POST['email']);
+        $password = sha1($_POST['password']);
+        $insertUser = $bdd->prepare('INSERT INTO USER(nom_user,mail,mdp)VALUES(?,?,?)');
+        $insertUser->execute(array($name,$email,$password));
+
+        $recupUser =$bdd->prepare('SELECT * FROM USER WHERE mail = ? and mdp = ?');
+        $recupUser->execute(array($email,$password));
+
+    
+        if ($recupUser->rowCount() > 0){
+                $_SESSION['mail'] = $email;
+                $_SESSION['mdp'] =  $password;
+                $_SESSION['id'] = $recupUser->fetch()['id'];
+            
+        }
+        
+        
+        else {
+            echo "Veuillez completer tous les champs...";
+            }
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="fr">
 
