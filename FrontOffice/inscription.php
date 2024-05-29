3102443be62277811836c9ddd7814f8ecf6a5756
matisu <150020403+MrRobboy@users.noreply.github.com>
2024-05-29 18:02:24 +0200

    a

--- FrontOffice/inscription.php
+++ FrontOffice/inscription.php
@@ -1,7 +1,7 @@
-<?php require_once '../FrontEnd/inscription.php'; ?>
+<?php include_once '../FrontEnd/inscription.php'; ?>
 <!DOCTYPE html>
 <html lang="fr">
-    
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
