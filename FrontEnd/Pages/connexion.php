2575be45cfd62fdf3e0763291686628ab3b81a0a
matisu <150020403+MrRobboy@users.noreply.github.com>
2024-05-30 12:24:36 +0200

    s

--- FrontEnd/Pages/connexion.php
+++ FrontEnd/Pages/connexion.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8" />
     <meta name ="viewport" content="width=device-width,initial-scale=1.0" />
     <title>Connexion | SchoolPéa</title>
-    <link rel="stylesheet" type="text/css" href="./style.css" />
+    <link rel="stylesheet" type="text/css" href="/Styles/style.css" />
 </head>
 
 <body>
@@ -40,5 +40,5 @@
             </div>
         </div>
     </div>
-    <script src="./script.js"></script><!--S'active que s'il est dans le body-->
+    <script src="../Scripts/script.js"></script><!--S'active que s'il est dans le body-->
 </body>
--- FrontEnd/Pages/inscription.php
+++ FrontEnd/Pages/inscription.php
@@ -3,14 +3,14 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
     <title>Inscription | SchoolPéa</title>
-    <link rel="stylesheet" type="text/css" href="./style.css" />
+    <link rel="stylesheet" type="text/css" href="../Styles/style.css" />
 </head>
 <body>
     <div class="container Inscription" id="Conteneur">
         <div class="form-container sign-in">
             <form action="../BackEnd/connexion.php" method="post">
                 <h1>Connexion</h1>
-                <input type="email" id="email" name="email" placeholder="eemail" required/>
+                <input type="email" id="email" name="email" placeholder="email" required/>
                 <input type="password" id="password" name="password" placeholder="Mot de passe" required/>
                 <a href="#">Mot de passe oublié ?</a>
                 <button type="submit" name="submit">Connexion</button>
@@ -40,6 +40,6 @@
             </div>
         </div>
     </div>
-    <script src="./script.js"></script>
+    <script src="../Scripts/script.js"></script>
 </body>
 </html>
