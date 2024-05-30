e75ab9990ddf73514a8b09270c17f08127f2c70e
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-05-30 14:30:23 +0200

    qsd

--- FrontEnd/Pages/logs.php
+++ FrontEnd/Pages/logs.php
@@ -2,7 +2,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
     <title>Logs</title>
-    <link rel="stylesheet" type="text/css" href="./logs.css" />
+    <link rel="stylesheet" type="text/css" href="../Styles/logs.css" />
 </head>
 
 <body>
