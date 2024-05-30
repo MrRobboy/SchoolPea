5f40be33dc8e96e991f7947b798980e50cc9a077
matisu <150020403+MrRobboy@users.noreply.github.com>
2024-05-30 16:30:14 +0200

    maybe

--- BackEnd/inscription.php
+++ BackEnd/inscription.php
@@ -1,5 +1,22 @@
 <?php
-include_once '../Includes/database.php';
+$host = 'localhost';
+$dbname = 'PA';
+$username = 'root';
+$password = 'root';
+$options = [
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+];
+
+try {
+    $bdd = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, $options);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //echo "Connexion Reussie ";
+
+} 
+catch (PDOException $e) {
+    echo "Erreur Connexion" . $e->getMessage();
+   
+}
 
 $error = false;
 $passwordError = false;
