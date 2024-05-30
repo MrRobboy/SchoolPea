95c95754abca757f846cea4f1c1c2001b41fec2a
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-05-30 14:29:06 +0200

    hop

--- FrontEnd/Pages/classement.php
+++ FrontEnd/Pages/classement.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
-<html lang="fr"></html>
+<html lang="fr">
+
+</html>
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
     <title>Classement</title>
-    <link rel="stylesheet" type="text/css" href="../../Styles/classement.css" />
+    <link rel="stylesheet" type="text/css" href="../Styles/classement.css" />
 </head>
 
 <body>
--- FrontEnd/Pages/compte.php
+++ FrontEnd/Pages/compte.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
     <title>Mon compte</title>
-    <link rel="stylesheet" type="text/css" href="../../Styles/compte.css" />
+    <link rel="stylesheet" type="text/css" href="../Styles/compte.css" />
 </head>
 
 
@@ -135,4 +136,5 @@
 
 
 </body>
+
 </html>
