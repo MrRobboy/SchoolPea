c867ba1f8eaa80a1b771e6ec8e7f70b1e732ff64
matisu <150020403+MrRobboy@users.noreply.github.com>
2024-05-30 13:33:30 +0200

    aze

--- BackEnd/inscription.php
+++ BackEnd/inscription.php
@@ -6,7 +6,7 @@ if(isset($_POST['submit'])){
     $email = $_POST['email'];
     $password =  password_hash($_POST['password'], PASSWORD_BCRYPT);
 
-    $requete = $bdd->prepare("INSERT INTO USER VALUES (0, :name, :email, :password)");
+    $requete = $bdd->prepare("INSERT INTO USER (name, email, password) VALUES (:name, :email, :password)");
     $requete->execute(
         array(
             "name" => $name,
