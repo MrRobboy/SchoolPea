05a2d2a64fc1e6216648cf4821fc806ee9de602c
matisu <150020403+MrRobboy@users.noreply.github.com>
2024-05-31 17:01:34 +0200

    c ciao

--- PHPMailer/envoi.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once('fonctions.php');
-
-if (isset($_POST['sujet']) && isset($_POST['message']) && isset($_POST['destination']))
-    sendmail($_POST['sujet'], $_POST['message'], $_POST['destination']);
-else {
-    header('Location: newsletter.php');
-    echo ("erreur de saise!");
-}
