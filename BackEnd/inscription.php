6a915c17229b530f21ef442de857659950b58f72
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-06-02 20:19:47 +0200

    test 2

--- BackEnd/inscription.php
+++ BackEnd/inscription.php
@@ -12,32 +12,30 @@ try {
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //echo "Connexion Reussie ";
 
-} 
-catch (PDOException $e) {
+} catch (PDOException $e) {
     echo "Erreur Connexion" . $e->getMessage();
-   
 }
 
 $error = false;
 $passwordError = false;
 
 // Vérifier si le formulaire d'inscription est soumis
-if(isset($_POST['submit'])) {
-    if ( isset($_POST['name']) && strlen($_POST['name']) < 2) {
+if (isset($_POST['submit'])) {
+    if (isset($_POST['name']) && strlen($_POST['name']) < 2) {
         $error = true;
     }
 
-    if ( isset($_POST['email']) && !preg_match("/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$/", $_POST['email']) ) {
+    if (isset($_POST['email']) && !preg_match("/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$/", $_POST['email'])) {
         $error = true;
     }
-    if ( isset($_POST['password']) && !preg_match('/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/', $_POST['password']) ) {
+    if (isset($_POST['password']) && !preg_match('/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/', $_POST['password'])) {
         $passwordError = true;
     }
 }
 
 if ($passwordError || $error) {
     // Vérifiez si le formulaire d'inscription est soumis
-    if(isset($_POST['submit'])) {
+    if (isset($_POST['submit'])) {
         $location = 'Location: ../FrontEnd/Pages/inscription.php?';
 
         if ($passwordError) {
@@ -49,7 +47,7 @@ if ($passwordError || $error) {
 }
 
 // Si le formulaire est soumis et aucune erreur n'est détectée, procédez à l'insertion des données dans la base de données
-if(isset($_POST['submit'])){
+if (isset($_POST['submit'])) {
     $name = $_POST['name'];
     $email = $_POST['email'];
     $password = $_POST['password'];
@@ -59,15 +57,14 @@ if(isset($_POST['submit'])){
 
     $sql = "
     INSERT INTO USER (name, email, password)
-    VALUES (:name, :email, :passwordHash);
+    VALUES ($name, $email, $passwordHash);
     ";
 
     $queryStatement = $dbh->prepare($sql);
 
     $queryStatement->execute([
-      "name" => $name,
-      "email" => $email,
-      "passwordHash" => $passwordHash // Utilisez la variable correcte pour le mot de passe haché
+        "name" => $name,
+        "email" => $email,
+        "passwordHash" => $passwordHash // Utilisez la variable correcte pour le mot de passe haché
     ]);
 }
-?>
