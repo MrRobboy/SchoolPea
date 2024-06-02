80442d0be16596e2c6e5967a2c5902abe9ed1b6e
Debian <debian@vps-c85689c2.vps.ovh.net>
2024-06-02 19:13:02 +0000

    test 3.0

--- BackEnd/inscription.php
+++ BackEnd/inscription.php
@@ -20,22 +20,22 @@ $error = false;
 $passwordError = false;
 
 // Vérifier si le formulaire d'inscription est soumis
-if (isset($_POST['submit'])) {
+if (isset($_POST['submit_inscription'])) {
     if (isset($_POST['name']) && strlen($_POST['name']) < 2) {
         $error = true;
     }
 
-    if (isset($_POST['email']) && !preg_match("/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$/", $_POST['email'])) {
+    if (isset($_POST['email_inscription']) && !preg_match("/^[\w\-\.]+@([\w-]+\.)+[\w-]{2,4}$/", $_POST['email_inscription'])) {
         $error = true;
     }
-    if (isset($_POST['password']) && !preg_match('/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/', $_POST['password'])) {
+    if (isset($_POST['password_inscription']) && !preg_match('/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/', $_POST['password_inscription'])) {
         $passwordError = true;
     }
 }
 
 if ($passwordError || $error) {
     // Vérifiez si le formulaire d'inscription est soumis
-    if (isset($_POST['submit'])) {
+    if (isset($_POST['submit_inscription'])) {
         $location = 'Location: ../FrontEnd/Pages/inscription.php?';
 
         if ($passwordError) {
@@ -47,26 +47,27 @@ if ($passwordError || $error) {
 }
 
 // Si le formulaire est soumis et aucune erreur n'est détectée, procédez à l'insertion des données dans la base de données
-if (isset($_POST['submit'])) {
+if (isset($_POST['submit_inscription'])) {
     $name = $_POST['name'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
-    echo ('INFOS : name ' . $_POST['name'] . 'mail' . $_POST['email'] . 'PWD : ' . $_POST['password']);
+    $email = $_POST['email_inscription'];
+    $password = $_POST['password_inscription'];
 
     // Utilisez password_hash pour hacher le mot de passe
-    $passwordHash = password_hash($_POST['password'], PASSWORD_BCRYPT);
+    $passwordHash = password_hash($password, PASSWORD_DEFAULT);
+
+    echo ('INFOS :<br>Name : ' . $name . '<br>Mail : ' . $email . '<br>Password : ' . $password.'<br>Password Hash : '. $passwordHash);
 
     $sql = "
     INSERT INTO USER (name, email, password)
-    VALUES (:name, :email, :passwordHash);
+    VALUES ($name, $email, $passwordHash);
     ";
     echo '<br> REUSSITE INJECTION';
 
     $queryStatement = $dbh->prepare($sql);
 
-    $queryStatement->execute([
+    $queryStatement->execute(/*[
         "name" => $name,
         "email" => $email,
         "passwordHash" => $passwordHash // Utilisez la variable correcte pour le mot de passe haché
-    ]);
+    ]*/);
 }
