de677afc34bdc6d2063f2a33dde7c6905591488b
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-06-02 21:12:30 +0200

    qsd

--- FrontEnd/Pages/inscription.php
+++ FrontEnd/Pages/inscription.php
@@ -1,20 +1,22 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Inscription | SchoolPéa</title>
     <link rel="stylesheet" type="text/css" href="../Styles/style.css">
 </head>
+
 <body>
     <div class="container Inscription" id="Conteneur">
         <div class="form-container sign-in">
             <form action="../../BackEnd/connexion.php" method="post">
                 <h1>Connexion</h1>
                 <?php
-                    if ($badCredentials) {
-                        echo('<p class="error">Mauvais identifiants</p>');
-                    }
+                if (isset($badCredentials) && $badCredentials) {
+                    echo ('<p class="error">Mauvais identifiants</p>');
+                }
                 ?>
                 <input type="email" id="email" name="email_connexion" placeholder="Email" required>
                 <input type="password" id="password" name="password_connexion" placeholder="Mot de passe" required>
@@ -25,9 +27,9 @@
 
         <div class="form-container sign-up">
             <?php
-                if (isset($_GET['error']) && $_GET['error'] === '1') {
-                    echo ('<p>Un champ a mal été saisi</p>');
-                }
+            if (isset($_GET['error']) && $_GET['password'] === '0') {
+                echo ('<p>Un champ a mal été saisi</p>');
+            }
             ?>
             <form action="../../BackEnd/inscription.php" method="post">
                 <h1 style="text-align: center">Bienvenue chez SchoolPéa</h1>
@@ -54,4 +56,5 @@
     </div>
     <script src="../Scripts/script.js"></script>
 </body>
+
 </html>
