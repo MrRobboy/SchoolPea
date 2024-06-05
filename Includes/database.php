2f8296ad0adaa54ce49f5a4bd435bcaf7b0f051b
Debian <debian@vps-c85689c2.vps.ovh.net>
2024-06-05 20:07:43 +0000

    dernière modif de la journée

--- Includes/database.php
+++ Includes/database.php
@@ -1,14 +1,10 @@
 <?php
-$host = 'localhost';
-$dbname = 'PA';
-$username = 'root';
-$password = 'root';
 $options = [
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
 ];
 
 try {
-    $bdd = new PDO("mysql:host=localhost;dbname=PA", $username, $password, $options);
+    $bdd = new PDO("mysql:host=localhost;dbaname=PA","root","root");
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     /*echo "Connexion Reussie ";*/
 } catch (PDOException $e) {
