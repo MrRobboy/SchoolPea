7704f8927235e4766bd6c286ba98e9173790972d
matisu <150020403+MrRobboy@users.noreply.github.com>
2024-06-06 17:31:55 +0200

    cours,quizz,creation cours pas encoree fini

---
+++ FrontEnd/Pages/creationCours.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Créer un cours en ligne</title>
+    <link rel="stylesheet" type="text/css" href="../Styles/style.css" />
+    
+</head>
+<body>
+    <h1>Créer un cours en ligne</h1>
+    <form action="ajouter_cours.php" method="post">
+        <label for="nom">Nom du cours:</label><br>
+        <input type="text" id="nom" name="nom" required><br>
+        <label for="niveau">Niveau du cours:</label><br>
+        <input type="text" id="niveau" name="niveau" required><br>
+        <label for="prix">Prix du cours:</label><br>
+        <input type="number" id="prix" name="prix" min="0" step="0.01" required><br>
+        <label for="createur">Créateur du cours:</label><br>
+        <input type="text" id="createur" name="createur" required><br><br>
+        <input type="submit" value="Ajouter le cours">
+        <label for="contenu">Contenu du cours:</label><br>
+        <textarea id="contenu" name="contenu" rows="4" cols="50" required></textarea><br>
+
+    </form>
+</body>
+</html>
