a16d81c50b7ceb28086e532334c8802c8248cb63
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-06-06 17:43:51 +0200

    VOILA

--- FrontEnd/Scripts/script_TEST_MATHIS.js
+++ FrontEnd/Scripts/script_TEST_MATHIS.js
@@ -1,18 +1,3 @@
-const Conteneur = document.getElementById("Conteneur");
-const InscriptionBtn = document.getElementById("Inscription");
-const ConnexionBtn = document.getElementById("Connexion");
-
-InscriptionBtn.addEventListener("click", () => {
-    document.title = "Inscription | SchoolPéa";
-    Conteneur.classList.add("Inscription");
-    Conteneur.classList.remove("Connexion");
-});
-
-ConnexionBtn.addEventListener("click", () => {
-    document.title = "Connexion | SchoolPéa";
-    Conteneur.classList.remove("Inscription");
-    Conteneur.classList.add("Connexion");
-});
 
 
 // Fonction pour gérer le clic sur l'image SchoolPea.png
