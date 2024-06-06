ace262321dc77b5b2965c8e270d6a124db2d0e6d
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-06-06 17:41:37 +0200

    putain casse les couilles

--- FrontEnd/Pages/connexion.php
+++ FrontEnd/Pages/connexion.php
@@ -2,10 +2,10 @@
 <html lang="fr">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Connexion | SchoolPéa</title>
-    <link rel="stylesheet" type="text/css" href="../Styles/style.css" />
+    <link rel="stylesheet" type="text/css" href="../Styles/style.css">
 </head>
 
 <body>
@@ -14,39 +14,38 @@
             <form action="../../BackEnd/connexion.php" method="post">
                 <h1>Connexion</h1>
                 <?php
-    if ($badCredentials) {
-        echo('<p class="error">Mauvais identidiants</p>');
-    }
-    ?>
-                <input type="email" id="email" name="email" placeholder="email" required />
-                <input type="password" id="password" name="password" placeholder="Mot de passe" required />
+                if (isset($badCredentials) && $badCredentials) {
+                    echo ('<p class="error">Mauvais identifiants</p>');
+                }
+                ?>
+                <input type="email" id="email" name="email_connexion" placeholder="Email" required>
+                <input type="password" id="password" name="password_connexion" placeholder="Mot de passe" required>
                 <a href="#">Mot de passe oublié ?</a>
-                <button type="submit" name="submit">Connexion</button>
+                <button type="submit" name="submit_connexion">Connexion</button>
             </form>
         </div>
 
         <div class="form-container sign-up">
-        <?php
-            if (isset($_GET['error']) && $_GET['error'] === 1) {
-                echo ('Un champ a mal été saisi');
-            }
-        ?>
+
             <form action="../../BackEnd/inscription.php" method="post">
                 <h1 style="text-align: center">Bienvenue chez SchoolPéa</h1>
-                <input type="text" id="name" name="name" placeholder="Name" />
-                <input type="email" id="email" name="email" placeholder="email" />
-                <input type="password" id="password" name="password" placeholder="Mot de passe" />
-                <p><?php if (isset($_GET['password']) && $_GET['password'] === '0') {
-                        echo ('Mauvais mdp');
-                    } ?></p>
-                <button type="submit">Inscription</button>
+                <?php
+                if (isset($_GET['error']) && $_GET['password'] === '0') {
+                    echo ('<p>Un champ a mal été saisi</p>');
+                }
+                ?>
+                <input type="text" id="name" name="name" placeholder="Nom" required>
+                <input type="email" id="email" name="email_inscription" placeholder="Email" required>
+                <input type="password" id="password" name="password_inscription" placeholder="Mot de passe" required>
+
+                <button type="submit" name="submit_inscription">Inscription</button>
             </form>
         </div>
 
         <div class="toggle-container">
             <div class="toggle">
                 <div class="toggle-panel toggle-left">
-                    <h1>Te revoila !</h1>
+                    <h1>Te revoilà !</h1>
                     <button class="hidden" id="Connexion">Connexion</button>
                 </div>
                 <div class="toggle-panel toggle-right">
