8a962b7cdac4930ba250b1db952962516efde3c9
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-06-19 14:43:12 +0200

    Eh voilàà

--- TEST/test.php
+++ TEST/test.php
@@ -1,3 +1,12 @@
 <?php
-echo ('<div style="margin-top:10em";><h1>Include reussi !</h1></div>');
+
 $x = 5;
+if ($x == 6) {
+    include_once('./headerL.php');
+    include_once('./test2.php'); /*Include c'est pour l'inclure, s'il ne trouve pas il va continuer à exécuter le code.
+    Require force la présence de ce qui est dans le chemin sinon il n'execute pas le code*/
+} else {
+    include_once('./test3.php');
+    include_once('headerNL.php');
+}
+echo 'Fin des conneries';
--- TEST/test3.php
+++ TEST/test3.php
@@ -1,3 +1,2 @@
 <?php
-
-echo ('<h1>MAMADOU AVAIT MAL AU DENTS</h1>');
+echo ('<div style="margin-top:10em";><h1>MAMADOU AVAIT MAL AU DENTS</h1></div>');
