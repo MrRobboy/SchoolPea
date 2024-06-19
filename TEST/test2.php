29dcfaa4f0b74f0c180de37a0f5c5ff9ad651648
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-06-19 09:58:52 +0200

    Test

---
+++ TEST/test2.php
@@ -0,0 +1,3 @@
+<?php
+include_once('./test.php'); /*Include c'est pour l'inclure, s'il ne trouve pas il va continuer à exécuter le code.
+Require force la présence de ce qui est dans le chemin sinon il n'execute pas le code*/
