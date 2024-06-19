46114f7b503a1c1dc989e6382f5f611ba3396085
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-06-19 10:36:13 +0200

    qsd

---
+++ TEST/headerL.php
@@ -0,0 +1,47 @@
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1.0" />
+    <link rel="stylesheet" type="text/css" href="./header.css" />
+</head>
+
+<header>
+    <div id="accueil">
+        <a href="#SchoolPea">
+            <img id="logo_header" src="../Images/SchoolPea.png" />
+        </a>
+        <a href="#SchoolPea"> SchoolPéa </a>
+    </div>
+    <div id="Pages">
+        <span>
+            <a class="lien_header" href="accueilNL.php"> SchoolPea+ </a>
+        </span>
+        <span>
+            <a class="lien_header" href="accueilNL.php">
+                Explorer les Quizzs
+            </a>
+        </span>
+        <span>
+            <a class="lien_header" href="accueilNL.php">
+                Explorer les Cours
+            </a>
+        </span>
+        <span>
+            <a class="lien_header" href="accueilNL.php">Mes Cours</a>
+        </span>
+
+        <span id="slide_down">
+            <svg xmlns="http://www.w3.org/2000/svg" widtd="30" height="30" fill="black" viewBox="0.5 .5 15 15">
+                <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5" />
+            </svg>
+            <div>
+                <a>Voir Plus</a>
+                <a>Mon compte</a>
+                <a>Paramètres</a>
+            </div>
+        </span>
+
+        <span style="margin-left: 1.2rem">
+            <img src="../Images/PP_TEST.jpg" style="width: 45px; border-radius: 50%" />
+        </span>
+    </div>
+</header>
