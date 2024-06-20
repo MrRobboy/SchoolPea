5f695af336ca219d3f9e0240cf644152333327a8
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-06-20 22:22:48 +0200

    qsdsqd

--- TEST/test.php
+++ TEST/test.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 $x = 5;
-if ($x == 5) {
+if ($x == 6) {
     include_once('./headerL.php');
     include_once('./test2.php'); /*Include c'est pour l'inclure, s'il ne trouve pas il va continuer à exécuter le code.
     Require force la présence de ce qui est dans le chemin sinon il n'execute pas le code*/
@@ -13,4 +13,4 @@ echo 'Fin des conneries';
 $_SESSION['x'] = $x;
 ?>
 <br>
-<a href="./SESSION_TEST.php">TEST1212</a>
+<a href="./logout.php">TEST1212</a>
