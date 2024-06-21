26d77c8ccefe8d8b1b5406b7420c4f09ece3d093
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-06-21 12:48:43 +0200

    eh voilà! mcd finito

--- TEST/test.php
+++ TEST/test.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
 $_SESSION['start'] = time();
-$x = 4;
-if ($x == 5) {
+$x = 5;
+if ($x >= 5) {
     include_once('./headerL.php');
     include_once('./test2.php'); /*Include c'est pour l'inclure, s'il ne trouve pas il va continuer à exécuter le code.
     Require force la présence de ce qui est dans le chemin sinon il n'execute pas le code*/
