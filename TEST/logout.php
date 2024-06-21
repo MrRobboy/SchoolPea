4f34aecb745ffcf88110e41b85e38943278c3d7d
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-06-21 12:56:00 +0200

    pst

--- TEST/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-if ($_SESSION['x']) echo ('1'); /* Vérifie que la session existe */
-session_unset();
-if ($_SESSION['x']) echo ('2'); /*Vide la session*/
-session_destroy(); /*Détruit la session */
