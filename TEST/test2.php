2fa9e0e2e38baa66039a9abd8c0b512320ed325d
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-06-21 13:02:28 +0200

    hop

---
+++ TEST/test2.php
@@ -0,0 +1,2 @@
+<?php
+echo ('<div style="margin-top:10em";><h1>Include reussi !</h1></div>');
---
+++ TEST/test3.php
@@ -0,0 +1,5 @@
+<?php
+echo ('<div style="margin-top:10em";><h1>MAMADOU AVAIT MAL AU DENTS</h1></div>');
+?>
+
+<a href="./SESSION_TEST.php">lien suivant</a>
