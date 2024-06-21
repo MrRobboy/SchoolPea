f08569e83d3eac4b8a17d65de505679bf3631321
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-06-21 22:50:08 +0200

    qsd

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
     <title>Schoolpéa</title>
-    <link rel="stylesheet" type="text/css" href="./TEST/header.css" />
+    <link rel="stylesheet" type="text/css" href="./header.css" />
     <link rel="stylesheet" type="text/css" href="./FrontEnd/Styles/accueilNL.css">
 </head>
 
