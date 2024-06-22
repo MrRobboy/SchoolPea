701e35e9fa6bc526167c72dd7d8b1d424dff334d
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-06-22 13:16:35 +0200

    qsd

--- Classement/index.php
+++ Classement/index.php
@@ -1,6 +1,7 @@
 <?php
 /*Changer le header selon s'il est connecté ou non.
 Afficher le classement en utilisant les données dans la BDD. (Créer des profils fake pour tester !)*/
+session_start();
 ?>
 <!DOCTYPE html>
 <html lang="fr">
@@ -13,47 +14,13 @@ Afficher le classement en utilisant les données dans la BDD. (Créer des profil
 </head>
 
 <body>
-        <header>
-                <div id="accueil">
-                        <a href="#SchoolPea">
-                                <img id="logo_header" src="../Images/SchoolPea.png" />
-                        </a>
-                        <a href="#SchoolPea"> SchoolPéa </a>
-                </div>
-                <div id="Pages">
-                        <span>
-                                <a class="lien_header" href="accueilNL.php"> SchoolPea+ </a>
-                        </span>
-                        <span>
-                                <a class="lien_header" href="accueilNL.php">
-                                        Explorer les Quizzs
-                                </a>
-                        </span>
-                        <span>
-                                <a class="lien_header" href="accueilNL.php">
-                                        Explorer les Cours
-                                </a>
-                        </span>
-                        <span>
-                                <a class="lien_header" href="accueilNL.php">Mes Cours</a>
-                        </span>
-
-                        <span id="slide_down">
-                                <svg xmlns="http://www.w3.org/2000/svg" widtd="30" height="30" fill="black" viewBox="0.5 .5 15 15">
-                                        <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5" />
-                                </svg>
-                                <div>
-                                        <a>Voir Plus</a>
-                                        <a>Mon compte</a>
-                                        <a>Paramètres</a>
-                                </div>
-                        </span>
-
-                        <span style="margin-left: 1.2rem">
-                                <img src="../Images/PP_TEST.jpg" style="width: 45px; border-radius: 50%" />
-                        </span>
-                </div>
-        </header>
+        <?php
+        if (isset($_SESSION['id'])) {
+                include_once('../headerL.php');
+        } else {
+                include_once('../headerNL.php');
+        }
+        ?>
 
         <span class="trait" id="SchoolPea"></span>
 
