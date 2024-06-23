60c3225377592498911d4be134cdaabb6e911801
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-06-23 20:31:41 +0200

    test 9999

--- BackEnd/Verif_captcha.php
+++ BackEnd/Verif_captcha.php
@@ -4,6 +4,15 @@ include('db.php');
 $request = $dbh->query('SELECT reponse1, reponse2, reponse3, reponse4, reponse5 FROM CAPTCHA where id=' . $_SESSION['x'] . ';');
 $reponses = $request->fetchAll();
 
+function verify($Answer_user, $Tab_Reponses)
+{
+    $Answer_user = stripslashes($Answer_user);
+    for ($index = 1; $index <= 5; $index++) $Tab_Answers[$index] = stripslashes($Tab_Reponses[0]['reponse' . $index]);
+    $result = false;
+    foreach ($Tab_Answers as $TA) if (strcasecmp(trim($Answer_user), trim($TA)) == 0) $result = true;
+    return $result;
+}
+
 if (!empty($_POST['submit'])) {
     if (verify($_POST['textCaptchaAnswer'], $reponses)) {
         unset($_SESSION['erreur']);
@@ -13,12 +22,3 @@ if (!empty($_POST['submit'])) {
         header('location: captcha.php');
     }
 } else header('location: captcha.php');
-
-function verify($Answer_user, $Tab_Reponses)
-{
-    $Answer_user = stripslashes($Answer_user);
-    for ($index = 1; $index <= 5; $index++) $Tab_Answers[$index] = stripslashes($Tab_Reponses[0]['reponse' . $index]);
-    $result = false;
-    foreach ($Tab_Answers as $TA) if (strcasecmp(trim($Answer_user), trim($TA)) == 0) $result = true;
-    return $result;
-}
