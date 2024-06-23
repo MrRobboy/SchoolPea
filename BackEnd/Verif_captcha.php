3feff58fa0922577840ad91342253dc135373a06
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-06-23 20:34:35 +0200

    test

--- BackEnd/Verif_captcha.php
+++ BackEnd/Verif_captcha.php
@@ -4,6 +4,16 @@ include('db.php');
 $request = $dbh->query('SELECT reponse1, reponse2, reponse3, reponse4, reponse5 FROM CAPTCHA where id=' . $_SESSION['x'] . ';');
 $reponses = $request->fetchAll();
 
+if (!empty($_POST['submit'])) {
+    if (!verify($_POST['textCaptchaAnswer'], $reponses)) {
+        $_SESSION['erreur'] = 'erreur';
+        header('location: captcha.php');
+    } else {
+        unset($_SESSION['erreur']);
+        header('location: ./message_verification.php');
+    }
+} else header('location: captcha.php');
+
 function verify($Answer_user, $Tab_Reponses)
 {
     $Answer_user = stripslashes($Answer_user);
@@ -12,14 +22,3 @@ function verify($Answer_user, $Tab_Reponses)
     foreach ($Tab_Answers as $TA) if (strcasecmp(trim($Answer_user), trim($TA)) == 0) $result = true;
     return $result;
 }
-
-if (!empty($_POST['submit'])) {
-    if (verify($_POST['textCaptchaAnswer'], $reponses)) {
-        unset($_SESSION['erreur']);
-        header('location: ./message_verification.php');
-    } else {
-        $_SESSION['erreur'] = 'erreur';
-        header('location: captcha.php');
-    }
-} 
-// else header('location: captcha.php');
