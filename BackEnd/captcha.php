9c38e2c7f79db31180d235ef425011ed12e2cefb
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-06-23 20:44:38 +0200

    mhhh

--- BackEnd/captcha.php
+++ BackEnd/captcha.php
@@ -9,9 +9,9 @@ $request = $dbh->query('SELECT question FROM CAPTCHA;');
 $questions = $request->fetchAll();
 $request = $dbh->query('SELECT count(id) FROM CAPTCHA;');
 $max = $request->fetchAll();
-$x = random_int(1, $max[0][0] - 1);
+$x = random_int(0, $max[0][0] - 1);
 $_SESSION['x'] = $x;
-$question = $questions[0]['question'];
+$question = $questions[$x]['question'];
 
 echo ('<form method="post" style="margin : 5rem auto; justify-content : center; display : flex; font-size: 1.5rem;" action="./Verif_captcha.php" >' . $question);
 echo ('<input name="textCaptchaAnswer" style="margin : 0 0.5rem; font-size : 1.5rem" type="text" required />');
