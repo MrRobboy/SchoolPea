066c76f47a0334d711c2b2035e029539e46d2c75
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-06-23 18:30:29 +0200

    test inscrs

--- BackEnd/inscription.php
+++ BackEnd/inscription.php
@@ -1,15 +1,15 @@
 <?php
-$options = [
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-];
+$dsn = 'mysql:host=localhost;dbname=PA;charset=utf8mb4';
+$username = 'root';
+$password = 'root';
 
+// Create a new PDO instance
 try {
-    $bdd = new PDO("mysql:host=localhost;dbaname=PA", "root", "root");
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connexion Reussie<br> ";
+    $dbh = new PDO($dsn, $username, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    echo "Erreur Connexion " . $e->getMessage();
-    die;
+    echo 'Connection failed: ' . $e->getMessage();
+    exit;
 }
 /*
 $error = false;
@@ -44,7 +44,8 @@ if ($passwordError || $error) {
 */
 
 if (isset($_POST['submit_inscription'])) {
-    $name = htmlspecialchars($_POST['name']);
+    $firstname = htmlspecialchars($_POST['firstname']);
+    $lastname = htmlspecialchars($_POST['lastname']);
     $email = htmlspecialchars($_POST['email_inscription']);
     $password = htmlspecialchars($_POST['password_inscription']);
 
@@ -52,9 +53,10 @@ if (isset($_POST['submit_inscription'])) {
 
     echo ('INFOS :<br>Name : ' . $name . '<br>Mail : ' . $email . '<br>Password : ' . $password . '<br>Password Hash : ' . $passwordHash);
 
-    $queryStatement = $bdd->prepare('USE PA; INSERT INTO USER(name, email, password) VALUES (:name, :email, :password);');
+    $queryStatement = $bdd->prepare('USE PA; INSERT INTO USER(firstname, lastname, email, password) VALUES (:firstname, :lastname, :email, :password);');
 
-    $queryStatement->bindvalue(':name', $name);
+    $queryStatement->bindvalue(':firstname', $firstname);
+    $queryStatement->bindvalue(':lastname', $lastname);
     $queryStatement->bindvalue(':email', $email);
     $queryStatement->bindvalue(':password', $passwordHash);
 
@@ -62,9 +64,8 @@ if (isset($_POST['submit_inscription'])) {
 
     if (isset($result) && !$result) {
         echo "<br><br>ECHEC INJECTION";
-       
     } else {
         echo "<br><br> REUSSITE INJECTION";
-        header('Location: ../FrontEnd/Pages/captcha.php');
+        // header('Location: ../FrontEnd/Pages/captcha.php');
     }
 }
