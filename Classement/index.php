8f306fca428332262f8dd10f727d97d733cc0b63
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-06-24 16:08:09 +0200

    mathis le golmon

--- Classement/index.php
+++ Classement/index.php
@@ -14,7 +14,7 @@ $result = $dbh->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Classement des Utilisateurs</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="./classement.css">
 </head>
 
 <body>
