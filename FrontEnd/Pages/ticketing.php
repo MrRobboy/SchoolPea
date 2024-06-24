6f81fe719df06abee7337261553fe129bd82919a
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-06-24 19:54:40 +0200

    hop

--- FrontEnd/Pages/ticketing.php
+++ FrontEnd/Pages/ticketing.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 
-
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
@@ -10,47 +9,11 @@
 
 
 <body>
-    <header>
-        <div id="accueil">
-
-            <img id="logo_header" src="../Images/SchoolPea.png" />
-            </a>
-            <a href="#SchoolPea"> SchoolPéa </a>
-        </div>
-        <div id="Pages">
-            <span>
-                <a class="lien_header" href="./index.html"> SchoolPea+ </a>
-            </span>
-            <span>
-                <a class="lien_header" href="./index.html">
-                    Explorer les Quizzs
-                </a>
-            </span>
-            <span>
-                <a class="lien_header" href="./index.html">
-                    Explorer les Cours
-                </a>
-            </span>
-            <span>
-                <a class="lien_header" href="./index.html">Mes Cours</a>
-            </span>
-
-            <span id="slide_down">
-                <svg xmlns="http://www.w3.org/2000/svg" widtd="30" height="30" fill="black" viewBox="0.5 .5 15 15">
-                    <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5m0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5" />
-                </svg>
-                <div>
-                    <a>Voir Plus</a>
-                    <a>Mon compte</a>
-                    <a>Paramètres</a>
-                </div>
-            </span>
-
-            <span style="margin-left: 1.2rem">
-                <img src="../Images/PP_TEST.jpg" style="width: 45px; border-radius: 50%" />
-            </span>
-        </div>
-    </header>
+    <?php
+    $path = $_SERVER['DOCUMENT_ROOT'];
+    $path .= '/headerNL.php';
+    include($path);
+    ?>
 
     <span class="trait" id="SchoolPea"></span>
 
