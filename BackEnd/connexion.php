aed4fcc674d0b6dded82c4f7a075dc48726f31d2
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-06-24 11:29:24 +0200

    C'est chiant de fou

--- BackEnd/connexion.php
+++ BackEnd/connexion.php
@@ -18,7 +18,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     echo $requestDB . '<br>';
     $UserInfo = $dbh->query($requestDB);
     $user = $UserInfo->fetchAll();
-    echo ('<pre>' . print_r($user[0]) . '</pre>');
+    echo ('<pre>');
+    print_r($user);
+    echo '</pre>';
     if (!empty($user) && $user[0]['validation_mail'] == true) {
         if (password_verify($password, $user[0]['password'])) {
             $_SESSION['id_user'] = htmlspecialchars($user[0]['id_user']);
