da5035f78a79061b46fc7ef166bd4a3cac3546d8
matisu <150020403+MrRobboy@users.noreply.github.com>
2024-06-25 17:27:09 +0200

    sdjsdji

--- Cours/explorerLesCours.php
+++ Cours/explorerLesCours.php
@@ -15,31 +15,34 @@ $cours = $query->fetchAll(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <title>Explorer les cours</title>
     <link rel="stylesheet" href="assets/style.css">
 </head>
+
 <body>
     <h1>Explorer les cours</h1>
     <!-- Formulaire de recherche -->
-    <form method="get" action="explorerLescours.php">
-        <input type="text" nom="search" placeholder="Rechercher des cours" value="<?= htmlspecialchars($search) ?>">
+    <form method="get" action="explorerLesCours.php">
+        <input type="text" name="search" placeholder="Rechercher des cours" value="<?= htmlspecialchars($search) ?>">
         <button type="submit">Rechercher</button>
     </form>
     <div class="cours">
-        <?php if (count($cours) > 0): ?>
-            <?php foreach ($cours as $cours): ?>
+        <?php if (count($cours) > 0) : ?>
+            <?php foreach ($cours as $cour) : ?>
                 <div class="cours">
-                    <img src="uploads/<?= $cours['image'] ?>" alt="<?= $cours['nom'] ?>">
-                    <h2><a href="cours.php?id=<?= $cours['id'] ?>"><?= $cours['nom'] ?></a></h2>
-                    <p>Niveau : <?= $cours['niveau'] ?></p>
-                    <p>Prix : <?= $cours['prix'] == 0 ? 'Gratuit' : $cours['prix'] . ' €' ?></p>
+                    <img src="uploads/<?= htmlspecialchars($cour['image']) ?>" alt="<?= htmlspecialchars($cour['nom']) ?>">
+                    <h2><a href="cours.php?id=<?= htmlspecialchars($cour['id']) ?>"><?= htmlspecialchars($cour['nom']) ?></a></h2>
+                    <p>Niveau : <?= htmlspecialchars($cour['niveau']) ?></p>
+                    <p>Prix : <?= $cour['prix'] == 0 ? 'Gratuit' : htmlspecialchars($cour['prix']) . ' €' ?></p>
                 </div>
             <?php endforeach; ?>
-        <?php else: ?>
+        <?php else : ?>
             <p>Aucun cours trouvé.</p>
         <?php endif; ?>
     </div>
 </body>
+
 </html>
