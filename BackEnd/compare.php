2591f66f3af05b91cc54c40e6ec80e57e8e45f63
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-06-26 16:03:42 +0200

    tests

--- BackEnd/compare.php
+++ BackEnd/compare.php
@@ -12,7 +12,7 @@ if (isset($_POST['submit'])) {
 		$result = $queryStatement->execute();
 		if ($result) {
 			$sqlRequest = 'SELECT id_USER FROM USER WHERE email =' . $_SESSION['email'];
-			$queryUser = $dbh->query($sqlRequest);
+			$queryUser = $dbh->prepare($sqlRequest);
 			$queryUser->execute();
 			$user_found = $queryUser->fetchAll();
 			if ($user_found) {
