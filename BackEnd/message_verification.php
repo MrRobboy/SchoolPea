e2a5ef2454f85d8036f4dad2963990d1a8efa2f0
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-06-26 14:19:56 +0200

    trop relou

--- BackEnd/message_verification.php
+++ BackEnd/message_verification.php
@@ -21,7 +21,7 @@ if (empty($_SESSION['mail_envoyee'])) {
 				<h1>Mail à valider !</h1>
 				<p>Un code de validation à saisir vous a été envoyé à l'adresse suivante :</p>
 				<p style="font-size: 20px; font-weight: 00;"><?php echo $_SESSION['email']; ?></p>
-				<input type="text" name="code" required>
+				<input type="text" name="code" style="font-size: 20px; font-weight: 400; background-color: #eee; border: solid 1px black;" required>
 				<button type="submit" name="submit">Valider</button>
 			</form>
 		</div>
