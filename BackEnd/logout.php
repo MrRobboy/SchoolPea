3008d64dfa043a58aeca545f5a636ccbc45c8ac8
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-06-26 16:37:26 +0200

    mybad

--- BackEnd/logout.php
+++ BackEnd/logout.php
@@ -4,7 +4,7 @@ include('db.php');
 $dbh->exec('USE PA');
 $message = $_SESSION['id_USER'] . ' - ' . $_SESSION['firstname'] . ' ' . $_SESSION['lastname'] . ' s\'est déconnecté !';
 $queryLogs = $dbh->prepare('INSERT INTO LOGS(id_user, act) VALUES (:id_USER,:msg);');
-$queryLogs->bindvalue(':id_USER', $user_found[0]['id_USER']);
+$queryLogs->bindvalue(':id_USER', $_SESSION['id_user']);
 $queryLogs->bindvalue(':msg', $message);
 $result2 = $queryLogs->execute();
 
