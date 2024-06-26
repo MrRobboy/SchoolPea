b8dd5a8ba4afae119689e84c64d63b0f05a466ca
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-06-26 15:59:31 +0200

    ah vasy nsm le sql

--- BackEnd/compare.php
+++ BackEnd/compare.php
@@ -5,18 +5,20 @@ include('db.php');
 echo (htmlspecialchars($_POST['code']) . '<br>' . $_SESSION['verif']);
 if (isset($_POST['submit'])) {
 	if (htmlspecialchars($_POST['code']) == $_SESSION['verif']) {
+		$dbh->exec('USE PA');
 		echo ('<br>code reussi !!');
-		$queryStatement = $dbh->prepare('USE PA; UPDATE USER SET validation_mail=1 WHERE email =:email;');
+		$queryStatement = $dbh->prepare('UPDATE USER SET validation_mail=1 WHERE email =:email;');
 		$queryStatement->bindvalue(':email', $_SESSION['email']);
 		$result = $queryStatement->execute();
 		if ($result) {
-			$sqlRequest = 'USE PA; SELECT id_USER FROM USER WHERE email =' . $_SESSION['email'];
+			$sqlRequest = 'SELECT id_USER FROM USER WHERE email =' . $_SESSION['email'];
 			$queryUser = $dbh->query($sqlRequest);
+			$queryUser->execute();
 			$user_found = $queryUser->fetchAll();
 			if ($user_found) {
-				$message = $_SESSION['email'] . ' a été validé par l\'utilisateur n°' . $id_USER;
+				$message = $_SESSION['email'] . ' a été validé par l\'utilisateur n°' . $user_found;
 				$queryLogs = $dbh->prepare('INSERT INTO LOGS(id_user, act) VALUES (:id_USER,:msg);');
-				$queryLogs->bindvalue(':id_USER', $id_USER);
+				$queryLogs->bindvalue(':id_USER', $user_found);
 				$queryLogs->bindvalue(':msg', $message);
 				$result2 = $queryLogs->execute();
 				echo ('<br>code reussi !!');
