d5864706a380dc6b7288e231d8ef0341d60e40f3
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-06-26 11:33:18 +0200

    testons

--- BackOffice/logs/index.php
+++ BackOffice/logs/index.php
@@ -13,27 +13,39 @@ echo ('<pre>');
 print_r($logs);
 echo ('</pre>');
 ?>
+<!DOCTYPE html>
+<html lang="fr">
 
-<div class="container" style="margin: auto;">
-    <h1>Visualisation des Logs</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>ID_LOGS</th>
-                <th>ID_Utilisateur</th>
-                <th>Action</th>
-                <th>Date</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php foreach ($logs as $log) : ?>
+<head>
+    <meta charset="UTF-8">
+    <title> LOGS </title>
+    <link rel="stylesheet" type="text/css" href="./classement.css">
+</head>
+
+<body>
+    <div class="container" style="margin: auto;">
+        <h1>Visualisation des Logs</h1>
+        <table>
+            <thead>
                 <tr>
-                    <td><?php echo $log['id_LOGS']; ?></td>
-                    <td><?php echo $log['id_user']; ?></td>
-                    <td><?php echo $log['act']; ?></td>
-                    <td><?php echo $log['time']; ?></td>
+                    <th>ID_LOGS</th>
+                    <th>ID_Utilisateur</th>
+                    <th>Action</th>
+                    <th>Date</th>
                 </tr>
-            <?php endforeach; ?>
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+                <?php foreach ($logs as $log) : ?>
+                    <tr>
+                        <td><?php echo $log['id_LOGS']; ?></td>
+                        <td><?php echo $log['id_user']; ?></td>
+                        <td><?php echo $log['act']; ?></td>
+                        <td><?php echo $log['time']; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</body>
+
+</html>
