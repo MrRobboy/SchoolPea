89c76efec035af06bd21afaa706ee00aa4085458
matisu <150020403+MrRobboy@users.noreply.github.com>
2024-06-27 20:08:56 +0200

    hop

--- Cours/unfollow.php
+++ Cours/unfollow.php
@@ -1,5 +1,11 @@
 <?php
+session_start();
 include 'db.php';
+
+if (!isset($_SESSION['user_id'])) {
+    echo "Vous devez être connecté pour ne plus suivre un cours.";
+    exit();
+}
 
 $id_cours = $_POST['id_cours'];
 $id_user = $_SESSION['user_id'];
@@ -8,7 +14,7 @@ $id_user = $_SESSION['user_id'];
 $sql = "DELETE FROM LIKES_COURS WHERE id_user = :id_user AND id_cours = :id_cours";
 $stmt = $dbh->prepare($sql);
 $stmt->bindParam(':id_user', $id_user);
-$stmt->bindParam(':id_cours', $id_cours);
+stmt->bindParam(':id_cours', $id_cours);
 $stmt->execute();
 
 echo "Cours supprimé des suivis.";
