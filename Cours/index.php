a49a7e478ddd0097188e666fc616024265f05098
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-06-29 18:39:32 +0200

    what the fuck

--- Cours/index.php
+++ Cours/index.php
@@ -39,7 +39,7 @@ $courses = $result->fetchAll(PDO::FETCH_ASSOC);
                 <?php foreach ($courses as $course) : ?>
                     <div class="course_item">
                         <h3><?php echo htmlspecialchars($course['nom']); ?></h3>
-                        <?php if (!empty($course['path_image_pres']) && file_exists($course['path_image_pres'])) : ?>
+                        <?php if (!empty($course['path_image_pres'])) : ?>
                             <img src="<?php echo htmlspecialchars($course['path_image_pres']); ?>" alt="Image de présentation">
                         <?php else : ?>
                             <img src="default-image.jpg" alt="Image par défaut">
