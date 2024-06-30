4387e37fbad5a6c68fa90ca634099c2ecc306f62
matisu <150020403+MrRobboy@users.noreply.github.com>
2024-06-30 15:16:49 +0200

    ea

--- chat/register.php
+++ chat/register.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include 'config.php';
+include 'db.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $email = $_POST['email'];
--- chat/send_message.php
+++ chat/send_message.php
@@ -1,12 +1,12 @@
 <?php
 session_start();
-include 'config.php';
+include 'db.php';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $message = $_POST['message'];
     $user_id = $_SESSION['user_id'];
 
-    $stmt = $conn->prepare("INSERT INTO messages (message, users_id) VALUES (:message, :user_id)");
+    $stmt = $dbh->prepare("INSERT INTO messages (message, users_id) VALUES (:message, :user_id)");
     $stmt->bindParam(':message', $message);
     $stmt->bindParam(':user_id', $user_id);
     $stmt->execute();
