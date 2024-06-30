69e9332c53a716fa473ea352f6af6ad0a212d0be
matisu <150020403+MrRobboy@users.noreply.github.com>
2024-06-30 16:48:43 +0200

    rohhh

--- messages/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $conn = new PDO("mysql:host=localhost;dbname=PA", 'root', 'root');
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-?>
