e893dacfa1621f2e8055369b449c3dd528ca5443
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-07-02 12:33:07 +0200

    ptn

--- BackOffice/User/apply_edit.php
+++ BackOffice/User/apply_edit.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-$_GET;
+$_POST;
 $auth = $_SERVER['DOCUMENT_ROOT'];
 $auth .= '/BackEnd/Includes/auth.php';
 include($auth);
@@ -9,23 +9,23 @@ $path .= '/BackEnd/db.php';
 include($path);
 
 echo '<pre>';
-print_r($_GET);
+print_r($_POST);
 echo '</pre>';
 
 $dbh->exec('USE PA');
 $stmt = $dbh->prepare("SELECT * FROM USER where email=:email");
-$stmt->bindvalue(':email', $_GET['email']);
+$stmt->bindvalue(':email', $_POST['email']);
 $stmt->execute();
 $userExist = $stmt->fetchAll();
 
-if ($userExist[0]['email'] != $_GET['email']) {
+if ($userExist[0]['email'] != $_POST['email']) {
 
     $stmt2 = $dbh->prepare("UPDATE USER SET lastname=:lastname, firstname=:firstname, email=:email, path_pp=:path_pp where id_USER = :id_user");
-    $stmt2->bindvalue(':id_user', $_GET['id_USER']);
-    $stmt2->bindvalue(':lastname', $_GET['lastname']);
-    $stmt2->bindvalue(':firstname', $_GET['firstname']);
-    $stmt2->bindvalue(':email', $_GET['email']);
-    $stmt2->bindvalue(':path_pp', $_GET['path_pp']);
+    $stmt2->bindvalue(':id_user', $_POST['id_USER']);
+    $stmt2->bindvalue(':lastname', $_POST['lastname']);
+    $stmt2->bindvalue(':firstname', $_POST['firstname']);
+    $stmt2->bindvalue(':email', $_POST['email']);
+    $stmt2->bindvalue(':path_pp', $_POST['path_pp']);
     $stmt2->execute();
     $result = $stmt2->fetchAll();
 
