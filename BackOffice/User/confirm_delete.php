af8e776c1f2c8fd2667660768721b651e9cdf5c7
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-07-02 15:01:03 +0200

    qsd

--- BackOffice/User/confirm_delete.php
+++ BackOffice/User/confirm_delete.php
@@ -25,8 +25,8 @@ include($path);
 <body style="padding: 5em;">
     <div style="margin: 0 auto; justify-content: center;align-items: center; text-align: center;">
         <h1 style="margin: 3em 0;">Supprimer l'utilisateur : <?php echo $_GET['id'] ?></h1>
-        <a style="text-align: center; margin:5em; padding: 0.5em 1em; font-size: 20px; font-weight: 600; text-decoration: none; background-color: red; color: white; border-radius: 1em;" href="delete.php?id=<?php echo $_GET['id']; ?>"> Supprimer l'utilisateur</a>
-        <a style="text-align: center; margin:5em; padding: 0.5em 1em; font-size: 20px; font-weight: 600; text-decoration: none; background-color: green; color: white; border-radius: 1em;" href="<?php echo $_SERVER['HTTP_REFERER']; ?>"> Annuler la suppression</a>
+        <a style="text-align: center; margin:5em; padding: 0.5em 1em; font-size: 20px; font-weight: 400; text-decoration: none; background-color: red; color: white; border-radius: 1em;" href="delete.php?id=<?php echo $_GET['id']; ?>"> Supprimer l'utilisateur</a>
+        <a style="text-align: center; margin:5em; padding: 0.5em 1em; font-size: 20px; font-weight: 400; text-decoration: none; background-color: green; color: white; border-radius: 1em;" href="<?php echo $_SERVER['HTTP_REFERER']; ?>"> Annuler la suppression</a>
     </div>
 </body>
 
