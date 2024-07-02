e12e63a9b2bfcec89df4766e8a84726f0a729db7
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-07-02 14:31:57 +0200

    ok test

--- BackOffice/User/delete.php
+++ BackOffice/User/delete.php
@@ -1,8 +1,14 @@
 <?php
 include '../includes/auth.php';
 include '../includes/functions.php';
+?>
+<script>
+    if (confirm("Êtes-vous sûr de vouloir supprimer cet utilisateur ?") !== true) {
+        window.location.href = document.referrer;
+    }
+</script>
 
-$id = $_GET['id'];
+<?php
 
 if (delete('users', $id)) {
     header('Location: index.php');
