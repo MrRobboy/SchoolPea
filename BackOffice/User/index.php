44cb87c409cbd95a05eebe58688ffed77c233a02
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-07-02 16:02:26 +0200

    qsd

--- BackOffice/User/index.php
+++ BackOffice/User/index.php
@@ -20,7 +20,8 @@ $users = $stmt->fetchAll();
 </head>
 
 <body style="padding-left: 10em;">
-    <?php if (!empty($_GET['success'])) echo '<p style="background-color: green; color: white; font-size: 40px; font-weight: 700; padding: 0.5em 1em; border-radius: 3em; text-align: center;">Utilisateur supprimé avec succès!</p>'; ?>
+    <?php if (!isset($_GET['success']) && $_GET['success'] == 1) echo '<p style="background-color: green; color: white; font-size: 40px; font-weight: 700; padding: 0.5em 1em; border-radius: 3em; text-align: center;">Utilisateur supprimé avec succès!</p>'; ?>
+    <?php if (!isset($_GET['success']) && $_GET['success'] == 1) echo '<p style="background-color: green; color: white; font-size: 40px; font-weight: 700; padding: 0.5em 1em; border-radius: 3em; text-align: center;">Utilisateur banni avec succès!</p>'; ?>
     <div id="content" style="width: 95%;">
         <h1 style="margin-bottom: 0.5em;">Gestion des Utilisateurs</h1>
         <a href="add.php" class="btn add">Ajouter un utilisateur</a>
