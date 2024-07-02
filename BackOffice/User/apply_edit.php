2a40533ec19c214165b0246bf7812fc7efec626d
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-07-02 14:01:52 +0200

    omg

--- BackOffice/User/apply_edit.php
+++ BackOffice/User/apply_edit.php
@@ -17,17 +17,21 @@ $dbh->exec('USE PA');
 $stmt = $dbh->prepare("SELECT * FROM USER where id_USER=:id");
 $stmt->bindvalue(':id', $_POST['id_USER']);
 $stmt->execute();
-$userChange = $stmt->fetchAll();
+$userInfo = $stmt->fetchAll();
 
-if ($userChange['email'] != $_POST['email']) {
+if ($userInfo['id_USER'] == $_POST['id_USER'] and $userInfo['email'] == $_POST['email'] and $userInfo['path_pp'] == $_POST['path_pp'] and $userInfo['firstname'] == $_POST['firstname'] and $userInfo['lastname'] == $_POST['lastname']) header('Location: ' . $_SERVER['HTTP_REFERER']);
+/*Ici il y a forcément eu une modification ! */
+if ($userInfo['email'] != $_POST['email']) {
     $dbh->exec('USE PA');
     $stmt1 = $dbh->prepare("SELECT * FROM USER where email=:email");
     $stmt1->bindvalue(':email', $_POST['email']);
     $stmt1->execute();
     $userExist = $stmt1->fetchAll();
-} else {
-    $_GET['error_mail'] = 1;
-    header('Location: ' . $_SERVER['HTTP_REFERER'] . '&error_mail=1');
+
+    if ($userExist['email'] == $_POST['email']) {
+        $_GET['error_mail'] = 1;
+        header('Location: ' . $_SERVER['HTTP_REFERER'] . '&error_mail=1');
+    }
 }
 
 $stmt2 = $dbh->prepare("UPDATE USER SET lastname=:lastname, firstname=:firstname, email=:email, path_pp=:path_pp where id_USER = :id_user");
@@ -38,6 +42,7 @@ $stmt2->bindvalue(':email', $_POST['email']);
 $stmt2->bindvalue(':path_pp', $_POST['path_pp']);
 $stmt2->execute();
 $result = $stmt2->fetchAll();
+
 
 if ($result) {
     echo 'success';
