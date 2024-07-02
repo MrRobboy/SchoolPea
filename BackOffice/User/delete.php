d1ed1e00551597d30d497530ea1076db1e908127
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-07-02 15:06:30 +0200

    test

---
+++ BackOffice/User/delete.php
@@ -0,0 +1,20 @@
+<?php
+session_start();
+$_GET;
+$auth = $_SERVER['DOCUMENT_ROOT'];
+$auth .= '/BackEnd/Includes/auth.php';
+include($auth);
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= '/BackEnd/db.php';
+include($path);
+
+$dbh->exec('USE PA');
+$stmt = $dbh->prepare("DELETE FROM USER where id_USER = :id_user");
+$stmt->bindvalue(':id_user', $_GET['id']);
+$stmt->execute();
+$result = $stmt->fetchAll();
+
+$path = '';
+if ($result)
+    $path = 'success=1';
+header('Location: https://schoolpea.com/BackOffice/User/index.php?' . $path);
