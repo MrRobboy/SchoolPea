671375505fc8ac30f73943cbd79703764ae419ff
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-07-02 15:22:43 +0200

    yes

--- BackOffice/User/delete.php
+++ BackOffice/User/delete.php
@@ -9,30 +9,26 @@ $path .= '/BackEnd/db.php';
 include($path);
 
 $dbh->exec('USE PA');
+
 $stmt1 = $dbh->prepare("DELETE FROM LOGS where id_USER = :id_user");
 $stmt1->bindvalue(':id_user', $_GET['id']);
-$stmt1->execute();
-$result1 = $stmt1->fetchAll();
+$result1 = $stmt1->execute();
 
 $stmt2 = $dbh->prepare("DELETE FROM NEWS where sent_by = :id_user");
 $stmt2->bindvalue(':id_user', $_GET['id']);
-$stmt2->execute();
-$result2 = $stmt2->fetchAll();
+$result2 = $stmt2->execute();
 
 $stmt3 = $dbh->prepare("DELETE FROM TICKET where id_admin = :id_user OR id_client=:id_user");
 $stmt3->bindvalue(':id_user', $_GET['id']);
-$stmt3->execute();
-$result3 = $stmt3->fetchAll();
+$result3 = $stmt3->execute();
 
 $stmt4 = $dbh->prepare("DELETE FROM messages where author = :id_user");
 $stmt4->bindvalue(':id_user', $_GET['id']);
-$stmt4->execute();
-$result4 = $stmt4->fetchAll();
+$result4 = $stmt4->execute();
 
 $stmt = $dbh->prepare("DELETE FROM USER where id_USER = :id_user");
 $stmt->bindvalue(':id_user', $_GET['id']);
-$stmt->execute();
-$result = $stmt->fetchAll();
+$result = $stmt->execute();
 
 if ($result) {
     header('Location: https://schoolpea.com/BackOffice/User/index.php?success=1');
