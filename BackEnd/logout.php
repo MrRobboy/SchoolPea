ae3c91784c41ab8110af556f8e6452713c9ef755
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-07-02 15:32:17 +0200

    qsd

--- BackEnd/logout.php
+++ BackEnd/logout.php
@@ -2,11 +2,19 @@
 session_start();
 include('db.php');
 $dbh->exec('USE PA');
-$message = $_SESSION['id_user'] . ' - ' . $_SESSION['firstname'] . ' ' . $_SESSION['lastname'] . ' s\'est déconnecté !';
-$queryLogs = $dbh->prepare('INSERT INTO LOGS(id_user, act) VALUES (:id_USER,:msg);');
+
+$queryLogs = $dbh->prepare('SELECT * FROM USER WHERE id_USER=:id_USER');
 $queryLogs->bindvalue(':id_USER', $_SESSION['id_user']);
 $queryLogs->bindvalue(':msg', $message);
-$result2 = $queryLogs->execute();
+$result1 = $queryLogs->execute();
+
+if ($result1[0]['id_USER'] == $_SESSION['id_USER']) {
+    $message = $_SESSION['id_user'] . ' - ' . $_SESSION['firstname'] . ' ' . $_SESSION['lastname'] . ' s\'est déconnecté !';
+    $queryLogs = $dbh->prepare('INSERT INTO LOGS(id_user, act) VALUES (:id_USER,:msg);');
+    $queryLogs->bindvalue(':id_USER', $_SESSION['id_user']);
+    $queryLogs->bindvalue(':msg', $message);
+    $result2 = $queryLogs->execute();
+}
 
 session_unset();
 session_destroy();
