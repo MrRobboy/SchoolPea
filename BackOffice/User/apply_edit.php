5d0c3c9a7dcc0b4f088079628a1e7d937833f42c
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-07-02 12:52:58 +0200

    ok

--- BackOffice/User/apply_edit.php
+++ BackOffice/User/apply_edit.php
@@ -13,29 +13,40 @@ print_r($_POST);
 echo '</pre>';
 
 $dbh->exec('USE PA');
-$stmt = $dbh->prepare("SELECT * FROM USER where email=:email");
-$stmt->bindvalue(':email', $_POST['email']);
+
+$stmt = $dbh->prepare("SELECT * FROM USER where id_USER=:id");
+$stmt->bindvalue(':id', $_POST['id_USER']);
 $stmt->execute();
-$userExist = $stmt->fetchAll();
+$userChange = $stmt->fetchAll();
 
-if ($userExist[0]['email'] != $_POST['email']) {
-
-    $stmt2 = $dbh->prepare("UPDATE USER SET lastname=:lastname, firstname=:firstname, email=:email, path_pp=:path_pp where id_USER = :id_user");
-    $stmt2->bindvalue(':id_user', $_POST['id_USER']);
-    $stmt2->bindvalue(':lastname', $_POST['lastname']);
-    $stmt2->bindvalue(':firstname', $_POST['firstname']);
-    $stmt2->bindvalue(':email', $_POST['email']);
-    $stmt2->bindvalue(':path_pp', $_POST['path_pp']);
-    $stmt2->execute();
-    $result = $stmt2->fetchAll();
-
-    if ($result) {
-        echo 'success';
-        echo $result;
-    } else {
-        echo 'echec';
-        echo $result;
-    }
+if ($userChange['email'] != $_POST['email']) {
+    $dbh->exec('USE PA');
+    $stmt1 = $dbh->prepare("SELECT * FROM USER where email=:email");
+    $stmt1->bindvalue(':email', $_POST['email']);
+    $stmt1->execute();
+    $userExist = $stmt1->fetchAll();
+} else {
+    $_GET['error_mail'] = 1;
+    header('Location: ' . $_SERVER['HTTP_REFERER'] . '&error_mail=1');
 }
-$_GET['error'] = 1;
-header('Location: ' . $_SERVER['HTTP_REFERER'] . '&error=1');
+
+$stmt2 = $dbh->prepare("UPDATE USER SET lastname=:lastname, firstname=:firstname, email=:email, path_pp=:path_pp where id_USER = :id_user");
+$stmt2->bindvalue(':id_user', $_POST['id_USER']);
+$stmt2->bindvalue(':lastname', $_POST['lastname']);
+$stmt2->bindvalue(':firstname', $_POST['firstname']);
+$stmt2->bindvalue(':email', $_POST['email']);
+$stmt2->bindvalue(':path_pp', $_POST['path_pp']);
+$stmt2->execute();
+$result = $stmt2->fetchAll();
+
+if ($result) {
+    echo 'success';
+    echo $result;
+    $_GET['success'] = 1;
+    header('Location: ' . $_SERVER['HTTP_REFERER'] . '&success=1');
+} else {
+    echo 'echec';
+    echo $result;
+    $_GET['error'] = 1;
+    header('Location: ' . $_SERVER['HTTP_REFERER'] . '&error=1');
+}
--- BackOffice/User/edit.php
+++ BackOffice/User/edit.php
@@ -30,7 +30,9 @@ $users = $stmt->fetchAll();
     $header .= '/BackOffice/Includes/headerBackOffice.php';
     include($header); ?>
 
-    <?php if (!empty($_GET['error'])) echo '<p style="background-color: red; color: white; font-size: 40px; font-weight: 700; padding: 0.5em 1em; border-radius: 3em; text-align: center;">MAIL DEJA EXISTANT!</p>'; ?>
+    <?php if (!empty($_GET['error_mail'])) echo '<p style="background-color: red; color: white; font-size: 40px; font-weight: 700; padding: 0.5em 1em; border-radius: 3em; text-align: center;">MAIL DEJA EXISTANT!</p>'; ?>
+    <?php if (!empty($_GET['error'])) echo '<p style="background-color: red; color: white; font-size: 40px; font-weight: 700; padding: 0.5em 1em; border-radius: 3em; text-align: center;">ERREUR VALEURS ENTREES!</p>'; ?>
+    <?php if (!empty($_GET['success'])) echo '<p style="background-color: green; color: white; font-size: 40px; font-weight: 700; padding: 0.5em 1em; border-radius: 3em; text-align: center;">REUSSITE CHANGEMENT</p>'; ?>
 
     <div id="div1">
         <form method="post" id="Info_gen" action="apply_edit.php">
