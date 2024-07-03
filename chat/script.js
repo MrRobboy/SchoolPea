7c7ba9462c52e2619ebb4a4b7ef6b8606d9890d7
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-07-03 21:55:40 +0200

    test

--- chat/script.js
+++ chat/script.js
@@ -1,3 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetchMessages(); // Appel de la fonction fetchMessages dès le chargement de la page
+    setInterval(fetchMessages, 3000); // Mise en place de l'intervalle
+
+    document.getElementById('message-form').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        const formData = new FormData(this);
+
+        fetch('sendMessage.php', {
+            method: 'POST',
+            body: formData
+        })
+        .then(response => response.text())
+        .then(data => {
+            fetchMessages(); 
+            this.reset(); 
+        })
+        .catch(error => console.error('Error sending message:', error));
+    });
+});
+
 function fetchMessages() {
     fetch('fetchMessages.php')
         .then(response => response.text())
@@ -6,26 +28,3 @@ function fetchMessages() {
         })
         .catch(error => console.error('Error fetching messages:', error));
 }
-
-
-
-setInterval(fetchMessages, 3000);
-
-
-document.getElementById('message-form').addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    const formData = new FormData(this);
-
-    fetch('sendMessage.php', {
-        method: 'POST',
-        body: formData
-    })
-    .then(response => response.text())
-    .then(data => {
-        setTimeout(fetchMessages,1);
-        fetchMessages(); 
-        this.reset(); 
-    })
-    .catch(error => console.error('Error sending message:', error));
-});
