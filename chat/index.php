085452d86ef58482ed6386065003741ee4b5bd32
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-07-03 22:04:00 +0200

    qsd

--- chat/index.php
+++ chat/index.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+include('checkUser.php');
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,25 +9,23 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Chat</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="chat.css">
 </head>
 
 <body>
     <?php
-    session_start(); // Assurez-vous que la session est démarrée
     $path = $_SERVER['DOCUMENT_ROOT'];
     if (isset($_SESSION['mail_valide'])) {
         $path .= '/headerL.php';
     } else {
         header('Location: https://schoolpea.com/Connexion');
-        exit(); // Assurez-vous de sortir du script après la redirection
     }
     include($path);
     ?>
 
     <div class="chat-container">
         <div class="messages" id="messages">
-            <!-- Messages seront chargés ici par JavaScript -->
+            <?php include 'fetch_messages.php'; ?>
         </div>
         <form action="send_message.php" method="post" id="message-form">
             <textarea name="content" required></textarea>
