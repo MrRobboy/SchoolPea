64581c1fde04faca33adbbd550fe917489d46045
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-07-03 22:57:31 +0200

    qsdqsd

---
+++ Chat/checkUser.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= '/BackEnd/db.php';
+include($path);
+
+function isUserLoggedIn()
+{
+    return isset($_SESSION['user_id']);
+}
+
+if (!isUserLoggedIn()) {
+    header("Location: https://schoolpea.com/Connexion");
+    exit();
+}
+
+$user_id = $_SESSION['user_id'];
+$sql = "SELECT email, path_pp FROM USER WHERE id_USER = :user_id";
+$stmt = $dbh->prepare($sql);
+$stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+$stmt->execute();
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
