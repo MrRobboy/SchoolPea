d1eca62df7184f24f815b3de3646f940ca6c2714
akhi_ahmed <158050544+elattarahmed@users.noreply.github.com>
2024-07-04 11:11:55 +0200

    mybad

--- Chat/fetchMessages.php
+++ Chat/fetchMessages.php
@@ -21,7 +21,7 @@ echo '</pre>';
         <div class="message">
             <img src="<?php echo 'https://schoolpea.com/' . htmlspecialchars($message['path_pp']); ?>" alt="Profile Picture">
             <div class="message-content" <?php if ($message['email'] == $_SESSION['email']) echo 'style="background-color: #00ffb39e"'; ?>>
-                <span><?php echo htmlspecialchars($message['email']); ?> <br> <?php echo $message['DATE_FORMAT(m.date_heure,"%e/%m/%Y %H:%i")']; ?></span>
+                <span><?php echo htmlspecialchars($message['email']); ?> <br> <?php echo $message["DATE_FORMAT(m.date_heure,'%e/%m/%Y %H:%i')"]; ?></span>
                 <p><?php echo $message['content']; ?></p>
             </div>
         </div>
