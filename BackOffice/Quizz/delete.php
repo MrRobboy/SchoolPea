d6f265392c049abbeea7364b8b8f9bc45d7292af
matisu <150020403+MrRobboy@users.noreply.github.com>
2024-07-05 18:15:16 +0200

    back

--- BackOffice/Quizz/delete.php
+++ BackOffice/Quizz/delete.php
@@ -1,13 +1,45 @@
 <?php
-include '../includes/auth.php';
-include '../includes/functions.php';
+session_start();
+$_GET;
+$auth = $_SERVER['DOCUMENT_ROOT'];
+$auth .= '/BackEnd/Includes/auth.php';
+include($auth);
+$path = $_SERVER['DOCUMENT_ROOT'];
+$path .= '/BackEnd/db.php';
+include($path);
 
-$id = $_GET['id'];
+$dbh->exec('USE PA');
 
-if (delete('quizzes', $id)) {
-    header('Location: index.php');
+$idQuiz = $_GET['id'];
+
+// Vérifier si l'utilisateur est connecté
+if (!isset($_SESSION['user_id'])) {
+    $_SESSION['redirect_url'] = $_SERVER['REQUEST_URI']; // Stocker l'URL actuelle
+    header("Location: login.php");
     exit();
-} else {
-    echo 'Erreur lors de la suppression du quizz';
 }
+
+// Récupérer le chemin de l'image associée au quiz
+$stmt = $dbh->prepare("SELECT path_image_pres FROM QUIZZ WHERE id_QUIZZ = :id");
+$stmt->bindValue(':id', $idQuiz);
+$result = $stmt->execute();
+$imagePath = $stmt->fetch(PDO::FETCH_ASSOC);
+
+// Supprimer le quiz de la base de données
+$stmt1 = $dbh->prepare("DELETE FROM QUIZZ WHERE id_QUIZZ = :id");
+$stmt1->bindValue(':id', $idQuiz);
+$result1 = $stmt1->execute();
+
+// Supprimer l'image associée au quiz, si elle existe
+if ($imagePath && file_exists($imagePath['path_image_pres'])) {
+    unlink($imagePath['path_image_pres']);
+}
+
+// Redirection en fonction du résultat de la suppression
+if ($result1) {
+    header('Location: https://schoolpea.com/BackOffice/Quizz/index.php?success=1');
+} else {
+    header('Location: https://schoolpea.com/BackOffice/Quizz');
+}
+exit();
 ?>
