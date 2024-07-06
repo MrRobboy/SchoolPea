88ffed3258ad63d2fc49c1f5f72d2819bb96b255
akhi_ahmed <mr.ahmed.elattar.pro@gmail.com>
2024-07-06 11:15:26 +0200

    tests

--- TEST/test.php
+++ TEST/test.php
@@ -6,7 +6,7 @@ echo '</pre>';
 if ($_FILES["image_pres"]["size"] > 0 && is_uploaded_file($_FILES["image_pres"]["tmp_name"])) {
         $target_dir = "/var/www/html/SchoolPea/TEST/uploads/";
         $target_file = $target_dir . basename($_FILES["image_pres"]["name"]);
-        move_uploaded_file($_FILES["image_pres"]["tmp_name"], $target_file);
+        move_uploaded_file($_FILES["pp_file"]["tmp_name"], $target_file);
 }
 
 echo 'POST<br><pre>';
