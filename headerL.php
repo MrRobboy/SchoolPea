f9a18d39a2f8c597e6c61b21e348795c59bf50e2
matisu <150020403+MrRobboy@users.noreply.github.com>
2024-07-06 20:06:19 +0200

    azeza

--- headerL.php
+++ headerL.php
@@ -80,11 +80,19 @@
         document.addEventListener('DOMContentLoaded', function () {
             const toggleSwitch = document.getElementById('dark-mode-toggle');
 
+            // Vérifie le localStorage pour l'état du mode sombre
+            if (localStorage.getItem('dark-mode') === 'enabled') {
+                document.body.classList.add('dark-mode');
+                toggleSwitch.checked = true;
+            }
+
             toggleSwitch.addEventListener('change', function () {
                 if (toggleSwitch.checked) {
                     document.body.classList.add('dark-mode');
+                    localStorage.setItem('dark-mode', 'enabled');
                 } else {
                     document.body.classList.remove('dark-mode');
+                    localStorage.setItem('dark-mode', 'disabled');
                 }
             });
         });
